7f0fa557dd3dd666e27ac8580b9cf3bd39032c26
knopa-boom <42748055+knopa-boom@users.noreply.github.com>
2019-02-06 01:39:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let x = document.getElementById("incomeBtn");
+let y = document.getElementById("modal");
+let z = document.getElementById("closeWindow");
+
+x.onclick = function openWindow() {
+    y.style="display:block";
+};
+
+z.onclick = function closeWindow() {
+    y.style="display:none";
+}
+
+
+
+
+
+
